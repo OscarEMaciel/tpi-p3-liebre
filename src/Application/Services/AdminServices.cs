a934cd540690bec9fc2c00f8a8a95414a9110ded
lucia <luuli.miranda1@gmail.com>
2024-10-23 15:19:48 -0300

    admin

---
+++ src/Application/Services/AdminServices.cs
@@ -0,0 +1,69 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Application.Models.Request;
+using Domain;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminService(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+        public IEnumerable<AdminDTO> GetAllAdmins()
+        {
+            var objs = _adminRepository.GetAll();
+            return AdminDTO.CreateList(objs);
+        }
+
+        public AdminDTO GetAdminById(int id)
+        {
+            var obj = _adminRepository.GetById(id)
+              ?? throw new NotFoundException(nameof(Admin), id);
+
+            return AdminDTO.Create(obj);
+        }
+
+        public void CreateAdmin(AdminCreateRequest adminCreteRequest)
+        {
+            var admin = new Admin(adminCreteRequest.Name, adminCreteRequest.LastName, adminCreteRequest.Email, adminCreteRequest.Username, adminCreteRequest.Password);
+
+            _adminRepository.Add(admin);
+        }
+
+        public void UpdateAdmin(int id, AdminUpdateRequest adminUpdateRequest)
+        {
+            var admin = _adminRepository.GetById(id)
+              ?? throw new NotFoundException(nameof(Admin), id);
+
+            if (adminUpdateRequest.Name != string.Empty) admin.Name = adminUpdateRequest.Name;
+
+            if (adminUpdateRequest.Email != string.Empty) admin.Email = adminUpdateRequest.Email;
+
+            if (adminUpdateRequest.Password != string.Empty) admin.Password = adminUpdateRequest.Password;
+
+            if (adminUpdateRequest.Username != string.Empty) admin.Username = adminUpdateRequest.Username;
+
+            if (adminUpdateRequest.LastName != string.Empty) admin.LastName = adminUpdateRequest.LastName;
+
+            _adminRepository.Update(admin);
+        }
+
+        public void DeleteAdmin(int id)
+        {
+            var admin = _adminRepository.GetById(id);
+            if (admin == null)
+            {
+                throw new NotFoundException(nameof(Admin), id);
+            }
+            _adminRepository.Delete(admin);
+        }
+    }
+}
