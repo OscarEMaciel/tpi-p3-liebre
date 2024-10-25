b99f49d3d5410a013bbc02b721aa61ce821a3d19
juuulii <julietamaragliano6@gmail.com>
2024-10-25 16:21:34 -0300

    ApplicationContext modificado

--- src/Infrastructure/Data/ApplicationContext.cs
+++ src/Infrastructure/Data/ApplicationContext.cs
@@ -16,7 +16,9 @@ namespace Infrastructure.Data
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<TransactionDetail> TransactionDetails { get; set; }
-        
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Item> Items { get; set; }
+
         public ApplicationContext(DbContextOptions<ApplicationContext>options): base (options) { }
 
 
