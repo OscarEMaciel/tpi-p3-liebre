5011595c60ce173099df6c7ac8db90066ea038b7
lucia <luuli.miranda1@gmail.com>
2024-10-25 13:32:34 -0300

    cambios en domain transaction

--- src/Domain/Entities/Transaction.cs
+++ src/Domain/Entities/Transaction.cs
@@ -1,32 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace Domain.Entities
 {
     public class Transaction
     {
-        public int Id { get; set; }  // Identificador único de la transacción
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
 
-        public decimal TotalAmount { get; set; }  // Total de la transacción
+        [Required]
+        public ICollection<TransactionDetail>? TransactionDetails { get; set; }
 
-        public DateTime Date { get; set; }  // Fecha de la transacción
+        public decimal Total { get; set; }
 
-        public string Seller { get; set; }  // Vendedor que realizó la transacción
-
-        // Relación con los artículos vendidos en la transacción
-        public ICollection<TransactionDetail> SoldItems { get; set; }
-
-        public Transaction()
+        public Transaction() { }
+        public Transaction(DateTime date, ICollection<TransactionDetail> transactionDetail, decimal total)
         {
-            SoldItems = new List<TransactionDetail>();  // Inicialización de la colección
-        }
-
-        public Transaction(decimal totalAmount, DateTime date, string seller, ICollection<TransactionDetail> soldItems)
-        {
-            TotalAmount = totalAmount;
             Date = date;
-            Seller = seller;
-            SoldItems = soldItems ?? new List<TransactionDetail>();
+            TransactionDetails = transactionDetail;
+            Total = total;
         }
     }
 }
